99d1352c9f38f8b5faeefd319d0a2800fb3f673f
Nemesis <shuklaakshatascom@gmail.com>
2022-01-17 17:15:04 +0530

    Changed package-name

---
+++ src/utils/VigenereCipher.js
@@ -0,0 +1,26 @@
+export function encrypt(text, key) {
+    if (!text || !key) return "";
+
+    const formattedKey = formatKey(key, text.length).toUpperCase();
+
+    return text.toUpperCase().split("").map((char, idx) => {
+        return String.fromCharCode(((char.charCodeAt(0) + formattedKey.charCodeAt(idx) - 130) % 26)  + 65);
+    }).join("");
+}
+
+export function decrypt(text, key) {
+    if (!text || !key) return "";
+
+    const formattedKey = formatKey(key, text.length).toUpperCase();
+
+    return text.toUpperCase().split("").map((char, idx) => {
+        return String.fromCharCode(((char.charCodeAt(0) - formattedKey.charCodeAt(idx)  + 26) % 26) + 65)
+    }).join("");
+}
+
+function formatKey(key, length) {
+    let times = Math.floor(length / key.length);
+    let remainder = length % key.length;
+
+    return key.repeat(times) + key.slice(0, remainder);
+}
